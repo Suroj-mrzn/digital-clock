e974eb070b1d5cda613c2a9e3ad515027313869a
Suroj-mrzn <surojmrzn112@gmail.com>
2026-06-10 14:13:59 +0545

    updated to 12hours format AM/PM

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 //Clock
 function updateClock(){
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, 0);
+    let hours = now.getHours();
+    //determine AM or PM
+    const ampm = hours >= 12 ? 'PM' : 'AM';     
+    // convert to 12 hr format
+    hours = hours % 12 || 12;
+    hours = hours ? hours : 12;
+    const hoursString = hours.toString().padStart(2, 0);
     const minutes = now.getMinutes().toString().padStart(2, 0);
     const seconds = now.getSeconds().toString().padStart(2, 0);
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    const timeString = `${hoursString}:${minutes}:${seconds} ${ampm}`;
     document.getElementById("clock").textContent = timeString;
 }
 
